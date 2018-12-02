a578067a7dd21083d2e802fb3762307b4547de1b
reparok <reparok@hotmail.com>
2018-12-02 13:36:09 +0700

    aa

---
+++ 1.js
@@ -0,0 +1,29 @@
+﻿ $(function(){
+ 	$('.nutmo').click(function(event) {
+ 		$(this).animate({
+ 			left:-50,
+ 			opacity: 0
+ 		});
+ 		$('.nutdong').animate({
+ 			left:20,
+ 			opacity:1
+ 		});
+ 		$('.nutdong i').addClass('xoay');
+ 		$('.nutmo i').removeClass('xoay');
+ 	});
+ 	$('.nutdong').click(function(event) {
+ 		$('.nutdong i').removeClass('xoay')
+ 		$('.nutmo i').addClass('xoay');
+ 		$(this).animate({
+ 			left:-50,
+ 			opacity:0
+ 		});
+ 		$('.nutmo').animate({
+ 			left:20,
+ 			opacity:1
+ 		});
+ 	});
+ 
+})  
+
+ 
